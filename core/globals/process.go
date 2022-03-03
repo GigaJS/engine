5f2f5ef21b6babdfde236242b7627730e70bbc4a
Nikita Kazancev <nganzikov@gmail.com>
2022-03-03 05:10:33 +0000

    Implement process.env()

--- core/globals/process.go
+++ core/globals/process.go
@@ -19,11 +19,23 @@ func (p *ProcessModule) cwd() goja.Value {
 	return p.runtime.ToValue(wd)
 }
 
+func (p *ProcessModule) env() goja.Value {
+	r := vm.NewObject()
+
+	for _, e := range os.Environ() {
+        pair := strings.SplitN(e, "=", 2)
+        _ = r.Set(pair[0], pair[1])
+    }
+
+	return p.runtime.ToValue(r)
+}
+
 func RegisterProcess(vm *goja.Runtime) {
 	p := &ProcessModule{runtime: vm}
 
 	o := vm.NewObject()
 	_ = o.Set("cwd", p.cwd)
+	_ = o.Set("env", p.env)
 
 	_ = vm.GlobalObject().Set("process", o)
 }
